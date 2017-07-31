039388c99ed89bc688d8b4839e6e0305c508dc54
freejoe76 <jmurphy@denverpost.com>
2017-07-31 18:47:16 -0400

    Add LIRR to the dicts" .

--- ingestmta/dicts.py
+++ ingestmta/dicts.py
@@ -2,5 +2,6 @@
 # Store data we use in this app
 
 lines = {
-    'subway': ['ALL','1','2','3','4','5','6','7','A','C','E','B','D','F','M','N','Q','R','J','Z','G','L','W']
+    'subway': ['ALL','1','2','3','4','5','6','7','A','C','E','B','D','F','M','N','Q','R','J','Z','G','L','W'],
+    'LIRR': ['Babylon','City Terminal Zone','Far Rockaway','Hempstead','Long Beach','Montauk','Oyster Bay','Port Jefferson','Port Washington','Ronkonkoma','West Hempstead']
 }
