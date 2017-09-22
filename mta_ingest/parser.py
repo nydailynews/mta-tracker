8007d9804e1ff1ab5d906339ee1ab93fe7ed082a
freejoe76 <jmurphy@denverpost.com>
2017-09-22 14:32:55 -0400

    More ignore strings

--- mta_ingest/parser.py
+++ mta_ingest/parser.py
@@ -177,7 +177,7 @@ Here's the legend for those symbols:
             # which is easier for us to parse than the list we had before.
             if text == '':
                 blank_count += 1
-            elif text not in ['We Apologize for the inconvenience.', 'Know Before You Go!!', 'Sign up for MY MTA Alerts at www.mymtaalerts.com', 'Know Before You Go!Sign up at MY MTA Alerts www.mymtaalerts.com', 'Know Before You Go.Sign up for My MTA Alerts at http://www.mymtaalerts.com', 'Allow additional travel time.We apologize for the inconvenience','Allow additional travel time.We apologize for any inconvenience.','Allow additional travel time.','Allow additional travel time. Know Before You Go!','Sign Up for My MTA Alerts at www.mymtaalerts.com', 'Allow additional travel time.We apologize for any inconvenience', 'Know Before You Go!Sign up for MY MTA Alerts at mymtaalerts.com']:
+            elif text not in ['Allow additional travel time', 'We Apologize for the inconvenience.', 'Know Before You Go!!', 'Sign up for MY MTA Alerts at www.mymtaalerts.com', 'Know Before You Go!Sign up at MY MTA Alerts www.mymtaalerts.com', 'Know Before You Go.Sign up for My MTA Alerts at http://www.mymtaalerts.com', 'Allow additional travel time.We apologize for the inconvenience','Allow additional travel time.We apologize for any inconvenience.','Allow additional travel time.','Allow additional travel time. Know Before You Go!','Sign Up for My MTA Alerts at www.mymtaalerts.com', 'Allow additional travel time.We apologize for any inconvenience', 'Know Before You Go!Sign up for MY MTA Alerts at mymtaalerts.com']:
                 blank_count = 0
                 if current_string != '':
                     current_string += ' '
