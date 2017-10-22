639f9609420111482faa502ce618e5872ee89721
freejoe76 <jmurphy@denverpost.com>
2017-10-22 13:56:51 -0400

    Start mapping out the URLs

--- dj/app/mta/views.py
+++ dj/app/mta/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def mode_index(request):
+    return HttpResponse('hi')    
+
+def mode_detail(request, mode):
+    return HttpResponse('hi')    
+
+def mode_archive_year(request):
+    return HttpResponse('hi')    
+
+def mode_archive_month(request):
+    return HttpResponse('hi')    
+
+def mode_archive_day(request):
+    return HttpResponse('hi')    
