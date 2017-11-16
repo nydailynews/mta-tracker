b9184a85c3caae7f547d676bcd0ff9b2398fb2fd
freejoe76 <jmurphy@denverpost.com>
2017-11-16 13:46:03 -0500

    Start writing the model

--- dj/app/mta/models.py
+++ dj/app/mta/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Mode(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return self
+
+    class Meta:
+        pass
+
+class Line(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, blank=True, null=True)
+    mode = models.ForeignKey(mode)
+
+    def get_absolute_url(self):
+        return self
+
+    class Meta:
+        pass
+
+class Alert(models.Model):
+    cause = models.TextField()
+    line = models.ForeignKey(line)
+    start = models.DateTimeField()
+    stop = models.DateTimeField(blank=True, null=True)
+
+class DelayAlert(Alert):
+    pass
