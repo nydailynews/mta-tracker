6dec4c1be7c15b34eec07207936a5b500878256b
freejoe76 <jmurphy@denverpost.com>
2017-11-16 14:26:10 -0500

    Basic admin

--- dj/app/mta/admin.py
+++ dj/app/mta/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.conf import settings
+from mta.models import Mode, Line, DelayAlert
 
-# Register your models here.
+class ModeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    save_on_top = True
+    prepopulated_fields = { 'slug': ('name',) }
+
+class LineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    save_on_top = True
+    prepopulated_fields = { 'slug': ('name',) }
+
+class DelayAlertAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Mode, ModeAdmin)
+admin.site.register(Line, LineAdmin)
+admin.site.register(DelayAlert, DelayAlertAdmin)
